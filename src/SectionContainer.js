505378564f2fccc138b8729ddf70ec4870c47d09
Robert Pedersen <rob@worldcrimeleague.org>
2019-08-19 18:12:17 -0400

    section

--- src/SectionContainer.js
+++ src/SectionContainer.js
@@ -7,7 +7,8 @@ class SectionContainer extends Component {
     super(props)
 
     this.state = {
-      classroom: []
+      classroom: [],
+      room: []
     }
   }
 
@@ -15,22 +16,31 @@ class SectionContainer extends Component {
   fetch(`http://localhost:3000/sections/1`)
   .then(res => res.json())
   .then(sections => {
-    debugger
     this.setState({
       classroom: sections,
+      room: sections.room
     })
   })
+  debugger
 }
 
   render() {
     return (
-      <div>
 
-      asdfas
+      <div className="card w-75">
+        <div className="card-body">
+          <h5 className="card-title">{this.state.classroom.name}</h5>
+          <p className="card-text">{this.state.classroom.description}</p>
+          <p className="card-text">Hours {this.state.classroom.start_hour} to {this.state.classroom.end_hour}</p>
+          <p className="card-text">Room {this.state.room.name} {this.state.room.room_number}</p>
 
+        </div>
       </div>
+
     )
   }
 }
 
 export default withRouter(SectionContainer)
+
+// <p className="card-text">Room: {this.state.classroom.room.name} {this.state.classroom.room.room_number}</p>
